047af03500d4ff0cd118dab31845ad7403696a65
Vicente Baixauli <vibaiher@gmail.com>
2015-11-30 20:17:55 +0100

    Fix Dog:index and add Dog:show

--- src/Ribercan/DogBundle/Controller/DogController.php
+++ src/Ribercan/DogBundle/Controller/DogController.php
@@ -8,6 +8,15 @@ class DogController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('RibercanDogBundle:Dog:index.html.twig', array());
+        $dogs = $this->get('doctrine')->getRepository('RibercanAdminDogBundle:Dog')->findAll();
+
+        return $this->render('RibercanDogBundle:Dog:index.html.twig', array('dogs' => $dogs));
+    }
+
+    public function showAction($id)
+    {
+        $dog = $this->get('doctrine')->getRepository('RibercanAdminDogBundle:Dog')->find($id);
+
+        return $this->render('RibercanDogBundle:Dog:show.html.twig', array('dog' => $dog));
     }
 }
