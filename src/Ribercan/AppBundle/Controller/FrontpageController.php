929da3175510693afeda946a37691f7483d953d0
Vicente Baixauli <vibaiher@gmail.com>
2016-08-04 11:33:21 +0000

    Use FilterForm in homepage

--- src/Ribercan/AppBundle/Controller/FrontpageController.php
+++ src/Ribercan/AppBundle/Controller/FrontpageController.php
@@ -5,18 +5,33 @@ namespace Ribercan\AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Ribercan\DogBundle\Form\DogFilterType;
 use Ribercan\DogBundle\Model\DogDecorator;
 
 class FrontpageController extends Controller
 {
     public function indexAction()
     {
+        $filterForm = $this->createForm(
+            DogFilterType::class,
+            null,
+            array(
+                'action' => $this->generateUrl('dogs_in_adoption'),
+                'method' => 'POST'
+            )
+        );
         $urgentAdoptions = $this->get('doctrine')->getRepository('RibercanAdminDogBundle:Dog')->findUrgentAdoptions();
         $decorated_dogs = array();
         foreach ($urgentAdoptions as $dog) {
             $decorated_dogs[] = new DogDecorator($dog);
         }
 
-        return $this->render('RibercanAppBundle:Frontpage:index.html.twig', array('urgent_adoptions' => $decorated_dogs));
+        return $this->render(
+            'RibercanAppBundle:Frontpage:index.html.twig',
+            array(
+                'filterForm' => $filterForm->createView(),
+                'urgent_adoptions' => $decorated_dogs
+            )
+        );
     }
 }
